044d7d7e055b9e4f64a224aa29e5c9adadc77831
irwantowibowotech <irwantowibowotech@gmail.com>
2024-04-09 19:13:52 +0700

    advent calender list

--- App.tsx
+++ App.tsx
@@ -1,16 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import DayListItem from "./src/components/core/DayListItem";
+
+const days = [...Array(24)].map((val, index) => index + 1);
 
 export default function App() {
-
-  console.log("Hello");
-
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <Text>Irwanto here</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <FlatList
+          contentContainerStyle={styles.content}
+          columnWrapperStyle={styles.column}
+          numColumns={2}
+          data={days}
+          renderItem={({item}) => (
+            <DayListItem day={item} />
+          )}
+        />
+      </View>
   );
 }
 
@@ -18,7 +24,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  content: {
+    gap: 10,
+    padding: 10,
+  },
+
+  column: {
+    gap: 10,
+  },
+
+  box: {
+    backgroundColor: '#F9EDE3',
+    flex: 1,
+    aspectRatio: 1/2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#9b4521',
+    borderRadius: 20,
+  },
+
+  text: {
+    color: '#9b4521',
+    fontSize: 70
+  }
 });
